1383cc58c4e3e41d765224000591070a84763242
Alireza Maleki <Alirezamalekiiiii78@gmail.com>
2021-10-21 14:38:32 +0330

    Add some items, databases, tables, sign up admin and on sample template

--- food/models.py
+++ food/models.py
@@ -3,6 +3,12 @@ from django.db.models.fields import CharField
 
 # Create your models here.
 class Item(models.Model):
+    
+    
+    def __str__(self):
+        return self.item_name
+    
+    
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
--- food/views.py
+++ food/views.py
@@ -1,9 +1,18 @@
+from django import template
+from django.db import models
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
+from django.template import context, loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    item_list = Item.objects.all()
+    template = loader.get_template("food/index.html")
+    context = {
+        
+    }
+    return HttpResponse(template.render(context, request))
 
 def item(request):
     return HttpResponse("These are the items.")
