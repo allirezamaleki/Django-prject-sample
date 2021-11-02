a16aff102221272cafa189d5b250a0dd9869e36e
Alireza Maleki <Alirezamalekiiiii78@gmail.com>
2021-11-02 14:43:25 +0330

    add and update the template files, add and provide static files such as a .css file and link them together

--- food/views.py
+++ food/views.py
@@ -6,14 +6,23 @@ from .models import Item
 from django.template import context, loader
 
 # Create your views here.
+
 def index(request):
     item_list = Item.objects.all()
-    template = loader.get_template("food/index.html")
+    # template = loader.get_template("food/index.html")
     context = {
-        
+        'item_list': item_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'food/index.html', context)
+    # return HttpResponse(template.render(context, request))
 
 def item(request):
     return HttpResponse("These are the items.")
 
+def detail(request, item_id):
+    item = Item.objects.get(pk = item_id)
+    context = {
+        'item': item,
+    }
+    # return HttpResponse("This is item no/id: %s" % item_id)
+    return render(request, 'food/detail.html', context)
