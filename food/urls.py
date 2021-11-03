2b5f1f87a490db0ecf1f1cf80303ee7629b2f515
Alireza Maleki <Alirezamalekiiiii78@gmail.com>
2021-11-03 18:59:35 +0330

    implement food update functionality

--- food/urls.py
+++ food/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('<int:item_id>/', views.detail, name = 'detail'),
     path('item/', views.item, name='item'),
     path('add/', views.create_item, name="create_item"),
+    #Edit
+    path('update/<int:id>/', views.update_item, name='update_item'),
     ]
